31952eab21b70647b220150dbb6059e3d332214c
Ziheng <zlia0050@student.monash.edu>
2025-10-30 00:05:51 +1100

    git conflict solved

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,41 +2,6 @@ import React, { useRef, useState } from "react";
 import "./App.css";
 
 const App = () => {
-<<<<<<< Updated upstream
-  const [visibleStart, setVisibleStart] = useState(0);
-  const [visibleEnd, setVisibleEnd] = useState(2);
-  const [sheetsLeft, setSheetsLeft] = useState(TOTAL_SHEETS);
-  const containerRef = useRef(null);
-
-  const handleScroll = (e) => {
-  const scrollTop = e.target.scrollTop;
-  const consumed = Math.floor(scrollTop / SHEET_HEIGHT);
-  const remaining = Math.max(TOTAL_SHEETS - consumed, 0);
-  setSheetsLeft(remaining);
-
-  // +1 ensures next sheet loads before you reach it
-  const end = Math.min(consumed + 3, TOTAL_SHEETS);
-  setVisibleEnd(end);
-};
-
-
-
-  const sheetsToShow = [];
-  for (let i = visibleStart; i < visibleEnd; i++) {
-    sheetsToShow.push(
-      <div
-        key={i}
-        className="sheet"
-        style={{
-          position: "absolute",
-          top: i * SHEET_HEIGHT, // ⬅ restore top positioning
-        }}
-      >
-        {/* <p className="sheet-text">{sheetTexts[i]}</p> */}
-      </div>
-    );
-  }
-=======
   const textPathRef = useRef(null);
   const animationRef = useRef(null);
   const [isAnimated, setIsAnimated] = useState(false);
@@ -87,7 +52,6 @@ const App = () => {
 
     setIsAnimated(!isAnimated);
   };
->>>>>>> Stashed changes
 
   return (
     <div className="app-container">
@@ -102,37 +66,6 @@ const App = () => {
           />
         </defs>
 
-<<<<<<< Updated upstream
-      <svg width="0" height="0">
-  <clipPath id="sheetClip" clipPathUnits="objectBoundingBox" transform="scale(0.0034, 0.0071)">
-    <path
-      d="
-        M 0,25
-        Q 145,0 290,25
-        L 290,125
-        Q 270,138 250,130
-        Q 230,138 210,130
-        Q 190,138 170,130
-        Q 150,138 130,130
-        Q 110,138 90,130
-        Q 70,138 50,130
-        Q 30,138 10,130
-        Q 0,135 0,125
-        Z
-      "
-    />
-  </clipPath>
-</svg>
-
-      <div
-        className="roll parallelogram" 
-        ref={containerRef}
-        onScroll={handleScroll}
-        style={{ position: "relative" }}
-      >
-        {/* Spacer defines total scroll height */}
-        <div style={{ height: TOTAL_SHEETS * SHEET_HEIGHT }}></div>
-=======
         <text
           fill="#ffffff"
           fontFamily="Roboto, sans-serif"
@@ -144,7 +77,6 @@ const App = () => {
           </textPath>
         </text>
       </svg>
->>>>>>> Stashed changes
 
       <button className="animate-button" onClick={handleAnimate}>
         {isAnimated ? "Animate Out" : "Let's Animate!"}
