0f4253f78ed191c5ce203e0430461204455a00af
Ziheng <zlia0050@student.monash.edu>
2025-11-03 02:00:46 +1100

    Experience template added

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./Navbar.jsx";
 import Intro from "./Intro.jsx";
+import Experience from "./Experience.jsx";
 import "./App.css";
 
 const App = () => {
@@ -8,6 +9,17 @@ const App = () => {
     <div className="app">
       <NavBar />
       <Intro />
+      <Experience />
+      
+      {/* <section id="projects" className="projects-section">
+        <h2>Projects</h2>
+        <p>Your projects go here...</p>
+      </section>
+
+      <section id="contact" className="contact-section">
+        <h2>Contact Me</h2>
+        <p>Your contact information goes here...</p>
+      </section> */}
     </div>
   );
 };
---
+++ imports/ui/Experience.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Experience.css";
+
+const Experience = () => {
+  return (
+    <section id="experience" className="experience-section">
+      <h2>Experience</h2>
+      {/* Add your experience content here */}
+      <p>Your work experience goes here...</p>
+    </section>
+  );
+};
+
+export default Experience;
