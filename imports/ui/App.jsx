4412954e5c1d0e3ccced4d3c25b6ab0dca3439d1
Ziheng <zlia0050@student.monash.edu>
2025-11-03 01:36:19 +1100

    Added a Navbar

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import NavBar from "./Navbar.jsx";
 import Intro from "./Intro.jsx";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="app">
+      <NavBar />
       <Intro />
     </div>
   );
