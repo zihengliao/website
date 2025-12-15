fc97dfafdf8e3980f03c6eab245b142dc6ad43bb
Ziheng <zlia0050@student.monash.edu>
2025-12-15 16:44:19 +1100

    vercel analytics

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx'; // adjust path if your App.jsx is nested
+import { Analytics } from "@vercel/analytics/react"
 
 // Wait for DOM to load
 const container = document.getElementById('root'); // CRA uses 'root' not 'react-target'
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+    <>
+    <App />
+    <Analytics />
+  </>
+);
